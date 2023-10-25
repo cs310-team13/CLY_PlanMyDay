23c28cd9b47d83033e67aaa7c856db1a16cf8478
Carolyn Luo <xluo0963@usc.edu>
2023-10-25 00:14:56 -0700

    added pop up images functionalities to feature 2

--- app/src/main/java/com/example/planmyday/LosAngelesActivity.java
+++ app/src/main/java/com/example/planmyday/LosAngelesActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,94 +14,226 @@ import java.util.ArrayList;
 public class LosAngelesActivity extends Activity {
 
     // Declare CheckBoxes for each attraction
-    private CheckBox checkBoxCrypto;
-    private CheckBox checkBoxDisney;
-    private CheckBox checkBoxDodgers;
-    private CheckBox checkBoxGriffith;
-    private CheckBox checkBoxHollywood;
-    private CheckBox checkBoxLAMOA;
-    private CheckBox checkBoxSantaMonica;
-    private CheckBox checkBoxBroad;
-    private CheckBox checkBoxGetty;
-    private CheckBox checkBoxGrove;
-    private CheckBox checkBoxUniversal;
-    private CheckBox checkBoxVenice;
+    private CheckBox checkbox_Crypto;
+    private CheckBox checkbox_Disney;
+    private CheckBox checkbox_Dodgers;
+    private CheckBox checkbox_Griffith;
+    private CheckBox checkbox_Hollywood;
+    private CheckBox checkbox_LACMA;
+    private CheckBox checkbox_SMP;
+    private CheckBox checkbox_Broad;
+    private CheckBox checkbox_Getty;
+    private CheckBox checkbox_Grove;
+    private CheckBox checkbox_Universal;
+    private CheckBox checkbox_Venice;
     // Button to submit choices
     private Button submitButton;
 
+    private ImageView popup_img;
+
+    private ImageView icon_Crypto;
+    private ImageView icon_Disney;
+    private ImageView icon_Dodgers;
+    private ImageView icon_Griffith;
+    private ImageView icon_Hollywood;
+    private ImageView icon_LACMA;
+    private ImageView icon_SMP;
+    private ImageView icon_Broad;
+    private ImageView icon_Getty;
+    private ImageView icon_Grove;
+    private ImageView icon_Universal;
+    private ImageView icon_Venice;
+
+    private boolean isPopupOpen = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_la_attractions);
 
-        checkBoxCrypto = findViewById(R.id.checkBoxCrypto);
-        checkBoxDisney = findViewById(R.id.checkBoxDisney);
-        checkBoxDodgers = findViewById(R.id.checkBoxDodgers);
-        checkBoxGriffith = findViewById(R.id.checkBoxGriffith);
-        checkBoxHollywood = findViewById(R.id.checkBoxHollywood);
-        checkBoxLAMOA = findViewById(R.id.checkBoxLAMOA);
-        checkBoxSantaMonica = findViewById(R.id.checkBoxSantaMonica);
-        checkBoxBroad = findViewById(R.id.checkBoxBroad);
-        checkBoxGetty = findViewById(R.id.checkBoxGetty);
-        checkBoxGrove = findViewById(R.id.checkBoxGrove);
-        checkBoxVenice = findViewById(R.id.checkBoxVenice);
-        checkBoxUniversal = findViewById(R.id.checkBoxUniversal);
+        checkbox_Crypto = findViewById(R.id.checkbox_Crypto);
+        checkbox_Disney = findViewById(R.id.checkbox_Disney);
+        checkbox_Dodgers = findViewById(R.id.checkbox_Dodgers);
+        checkbox_Griffith = findViewById(R.id.checkbox_Griffith);
+        checkbox_Hollywood = findViewById(R.id.checkbox_Hollywood);
+        checkbox_LACMA = findViewById(R.id.checkbox_LACMA);
+        checkbox_SMP = findViewById(R.id.checkbox_SMP);
+        checkbox_Broad = findViewById(R.id.checkbox_Broad);
+        checkbox_Getty = findViewById(R.id.checkbox_Getty);
+        checkbox_Grove = findViewById(R.id.checkbox_Grove);
+        checkbox_Universal = findViewById(R.id.checkbox_Universal);
+        checkbox_Venice = findViewById(R.id.checkbox_Venice);
 
         submitButton = findViewById(R.id.submitButton);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an ArrayList to store selected attractions
-                ArrayList<String> LAAttractions = new ArrayList<>();
+        submitButton.setOnClickListener(v -> {
+            // Create an ArrayList to store selected attractions
+            ArrayList<String> LAAttractions = new ArrayList<>();
 
-                // Check each checkbox and add the selected attractions to the list
-                if (checkBoxCrypto.isChecked()) {
-                    LAAttractions.add("Crypto.com Arena");
-                }
-                if (checkBoxDisney.isChecked()) {
-                    LAAttractions.add("Disneyland Park");
-                }
-                if (checkBoxDodgers.isChecked()) {
-                    LAAttractions.add("Dodgers Stadium");
-                }
-                if (checkBoxGriffith.isChecked()) {
-                    LAAttractions.add("Griffith Observatory");
-                }
-                if (checkBoxHollywood.isChecked()) {
-                    LAAttractions.add("Hollywood Sign");
-                }
-                if (checkBoxLAMOA.isChecked()) {
-                    LAAttractions.add("Los Angeles County Museum of Art");
-                }
-                if (checkBoxSantaMonica.isChecked()) {
-                    LAAttractions.add("Santa Monica Pier");
-                }
-                if (checkBoxBroad.isChecked()) {
-                    LAAttractions.add("The Broad");
-                }
-                if (checkBoxGetty.isChecked()) {
-                    LAAttractions.add("The Getty");
-                }
-                if (checkBoxGrove.isChecked()) {
-                    LAAttractions.add("The Grove");
-                }
-                if (checkBoxUniversal.isChecked()) {
-                    LAAttractions.add("Universal Studios Hollywood");
-                }
-                if (checkBoxVenice.isChecked()) {
-                    LAAttractions.add("Venice Beach");
-                }
+            // Check each checkbox and add the selected attractions to the list
+            if (checkbox_Crypto.isChecked()) {
+                LAAttractions.add("Crypto.com Arena");
+            }
+            if (checkbox_Disney.isChecked()) {
+                LAAttractions.add("Disneyland Park");
+            }
+            if (checkbox_Dodgers.isChecked()) {
+                LAAttractions.add("Dodgers Stadium");
+            }
+            if (checkbox_Griffith.isChecked()) {
+                LAAttractions.add("Griffith Observatory");
+            }
+            if (checkbox_Hollywood.isChecked()) {
+                LAAttractions.add("Hollywood Sign");
+            }
+            if (checkbox_LACMA.isChecked()) {
+                LAAttractions.add("Los Angeles County Museum of Art");
+            }
+            if (checkbox_SMP.isChecked()) {
+                LAAttractions.add("Santa Monica Pier");
+            }
+            if (checkbox_Broad.isChecked()) {
+                LAAttractions.add("The Broad");
+            }
+            if (checkbox_Getty.isChecked()) {
+                LAAttractions.add("The Getty");
+            }
+            if (checkbox_Grove.isChecked()) {
+                LAAttractions.add("The Grove");
+            }
+            if (checkbox_Universal.isChecked()) {
+                LAAttractions.add("Universal Studios Hollywood");
+            }
+            if (checkbox_Venice.isChecked()) {
+                LAAttractions.add("Venice Beach");
+            }
 
-                if (LAAttractions.isEmpty()) {
-                    // Show a warning or toast message to the user
-                    Toast.makeText(LosAngelesActivity.this, "Please select at least one attraction.", Toast.LENGTH_SHORT).show();
-                } else {
-                    // At least one attraction is selected, proceed to the next activity
-                    Intent intent = new Intent(LosAngelesActivity.this, DaySelectionActivity.class);
-                    intent.putStringArrayListExtra("LAAttractions", LAAttractions);
-                    startActivity(intent);
-                }
+            if (LAAttractions.isEmpty()) {
+                // Show a warning or toast message to the user
+                Toast.makeText(LosAngelesActivity.this, "Please select at least one attraction.", Toast.LENGTH_SHORT).show();
+            } else {
+                // At least one attraction is selected, proceed to the next activity
+                Intent intent = new Intent(LosAngelesActivity.this, DaySelectionActivity.class);
+                intent.putStringArrayListExtra("LAAttractions", LAAttractions);
+                startActivity(intent);
+            }
+        });
+
+        icon_Crypto = findViewById(R.id.icon_Crypto);
+        icon_Disney = findViewById(R.id.icon_Disney);
+        icon_Dodgers = findViewById(R.id.icon_Dodgers);
+        icon_Griffith = findViewById(R.id.icon_Griffith);
+        icon_Hollywood = findViewById(R.id.icon_Hollywood);
+        icon_LACMA = findViewById(R.id.icon_LACMA);
+        icon_SMP = findViewById(R.id.icon_SMP);
+        icon_Broad = findViewById(R.id.icon_Broad);
+        icon_Getty = findViewById(R.id.icon_Getty);
+        icon_Grove = findViewById(R.id.icon_Grove);
+        icon_Universal = findViewById(R.id.icon_Universal);
+        icon_Venice = findViewById(R.id.icon_Venice);
+
+        popup_img = findViewById(R.id.popup_img);
+
+        icon_Crypto.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.crypto);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Disney.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.disney);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Dodgers.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.dodger);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Griffith.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.griffith);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Hollywood.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.hollywood);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_LACMA.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.lacma);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_SMP.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.smp);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Broad.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.broad);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Getty.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.getty);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Grove.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.grove);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Universal.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.universal);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Venice.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.venice);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+
+        popup_img.setOnClickListener(v -> {
+            if (isPopupOpen) {
+                popup_img.setVisibility(View.INVISIBLE);
+                isPopupOpen = false;
             }
         });
     }
--- app/src/main/java/com/example/planmyday/USCActivity.java
+++ app/src/main/java/com/example/planmyday/USCActivity.java
@@ -6,46 +6,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class USCActivity extends Activity {
-
-    // Declare CheckBoxes for each attraction
-    private CheckBox checkBoxAnnenberg;
-    private CheckBox checkBoxDoheny;
-    private CheckBox checkBoxGwynnWilson;
-    private CheckBox checkBoxKDC;
-    private CheckBox checkBoxLeavey;
-    private CheckBox checkBoxMudd;
-    private CheckBox checkBoxPE;
-    private CheckBox checkBoxSalvatori;
-    private CheckBox checkBoxTaper;
-    private CheckBox checkBoxTommyTrojan;
-    private CheckBox checkBoxTravelerHorse;
-    private CheckBox checkBoxUSCBookstore;
+    private CheckBox checkbox_Annenberg;
+    private CheckBox checkbox_Doheny;
+    private CheckBox checkbox_GW;
+    private CheckBox checkbox_KDC;
+    private CheckBox checkbox_Leavey;
+    private CheckBox checkbox_Mudd;
+    private CheckBox checkbox_PE;
+    private CheckBox checkbox_SAL;
+    private CheckBox checkbox_THH;
+    private CheckBox checkbox_Tommy;
+    private CheckBox checkbox_Traveler;
+    private CheckBox checkbox_Bookstore;
 
     // Button to submit choices
     private Button submitButton;
+
+    // pop up images
+    private ImageView popup_img;
+
+    // icons
+    private ImageView icon_Annenberg;
+    private ImageView icon_Doheny;
+    private ImageView icon_GW;
+    private ImageView icon_KDC;
+    private ImageView icon_Leavey;
+    private ImageView icon_Mudd;
+    private ImageView icon_PE;
+    private ImageView icon_SAL;
+    private ImageView icon_THH;
+    private ImageView icon_Tommy;
+    private ImageView icon_Traveler;
+    private ImageView icon_Bookstore;
+
+    private boolean isPopupOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usc_attractions);
 
-        checkBoxAnnenberg = findViewById(R.id.checkBoxAnnenberg);
-        checkBoxDoheny = findViewById(R.id.checkBoxDoheny);
-        checkBoxGwynnWilson = findViewById(R.id.checkBoxGwynnWilson);
-        checkBoxKDC = findViewById(R.id.checkBoxKDC);
-        checkBoxLeavey = findViewById(R.id.checkBoxLeavey);
-        checkBoxMudd = findViewById(R.id.checkBoxMudd);
-        checkBoxPE = findViewById(R.id.checkBoxPE);
-        checkBoxSalvatori = findViewById(R.id.checkBoxSalvatori);
-        checkBoxTaper = findViewById(R.id.checkBoxTaper);
-        checkBoxTommyTrojan = findViewById(R.id.checkBoxTommyTrojan);
-        checkBoxTravelerHorse = findViewById(R.id.checkBoxTravelerHorse);
-        checkBoxUSCBookstore = findViewById(R.id.checkBoxUSCBookstore);
+        checkbox_Annenberg = findViewById(R.id.checkbox_Annenberg);
+        checkbox_Doheny = findViewById(R.id.checkbox_Doheny);
+        checkbox_GW = findViewById(R.id.checkbox_GW);
+        checkbox_KDC = findViewById(R.id.checkbox_KDC);
+        checkbox_Leavey = findViewById(R.id.checkbox_Leavey);
+        checkbox_Mudd = findViewById(R.id.checkbox_Mudd);
+        checkbox_PE = findViewById(R.id.checkbox_PE);
+        checkbox_SAL = findViewById(R.id.checkbox_SAL);
+        checkbox_THH = findViewById(R.id.checkbox_THH);
+        checkbox_Tommy = findViewById(R.id.checkbox_Tommy);
+        checkbox_Traveler = findViewById(R.id.checkbox_Traveler);
+        checkbox_Bookstore = findViewById(R.id.checkbox_Bookstore);
 
         submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -55,40 +73,40 @@ public class USCActivity extends Activity {
                 ArrayList<String> USCAttractions = new ArrayList<>();
 
                 // Check each checkbox and add the selected attractions to the list
-                if (checkBoxAnnenberg.isChecked()) {
+                if (checkbox_Annenberg.isChecked()) {
                     USCAttractions.add("Annenberg School of Communication and Journalism");
                 }
-                if (checkBoxDoheny.isChecked()) {
+                if (checkbox_Doheny.isChecked()) {
                     USCAttractions.add("Doheny Memorial Library");
                 }
-                if (checkBoxGwynnWilson.isChecked()) {
+                if (checkbox_GW.isChecked()) {
                     USCAttractions.add("Gwynn Wilson Student Union");
                 }
-                if (checkBoxKDC.isChecked()) {
+                if (checkbox_KDC.isChecked()) {
                     USCAttractions.add("Kaufman International Dance Center");
                 }
-                if (checkBoxLeavey.isChecked()) {
+                if (checkbox_Leavey.isChecked()) {
                     USCAttractions.add("Leavey Library");
                 }
-                if (checkBoxMudd.isChecked()) {
+                if (checkbox_Mudd.isChecked()) {
                     USCAttractions.add("Mudd Hall of Philosophy");
                 }
-                if (checkBoxPE.isChecked()) {
+                if (checkbox_PE.isChecked()) {
                     USCAttractions.add("Physical Education Building");
                 }
-                if (checkBoxSalvatori.isChecked()) {
+                if (checkbox_SAL.isChecked()) {
                     USCAttractions.add("Salvatori Computer Science Center");
                 }
-                if (checkBoxTaper.isChecked()) {
+                if (checkbox_THH.isChecked()) {
                     USCAttractions.add("Taper Hall of Humanities");
                 }
-                if (checkBoxTommyTrojan.isChecked()) {
+                if (checkbox_Tommy.isChecked()) {
                     USCAttractions.add("Tommy Trojan Sculpture");
                 }
-                if (checkBoxTravelerHorse.isChecked()) {
+                if (checkbox_Traveler.isChecked()) {
                     USCAttractions.add("Traveler the Horse Sculpture");
                 }
-                if (checkBoxUSCBookstore.isChecked()) {
+                if (checkbox_Bookstore.isChecked()) {
                     USCAttractions.add("USC Bookstore");
                 }
 
@@ -101,6 +119,125 @@ public class USCActivity extends Activity {
                     intent.putStringArrayListExtra("USCAttractions", USCAttractions);
                     startActivity(intent);
                 }
+            }
+        });
+
+        icon_Annenberg = findViewById(R.id.icon_Annenberg);
+        icon_Doheny = findViewById(R.id.icon_Doheny);
+        icon_GW = findViewById(R.id.icon_GW);
+        icon_KDC = findViewById(R.id.icon_KDC);
+        icon_Leavey = findViewById(R.id.icon_Leavey);
+        icon_Mudd = findViewById(R.id.icon_Mudd);
+        icon_PE = findViewById(R.id.icon_PE);
+        icon_SAL = findViewById(R.id.icon_SAL);
+        icon_THH = findViewById(R.id.icon_THH);
+        icon_Tommy = findViewById(R.id.icon_Tommy);
+        icon_Traveler = findViewById(R.id.icon_Traveler);
+        icon_Bookstore = findViewById(R.id.icon_Bookstore);
+
+        popup_img = findViewById(R.id.popup_img);
+
+        icon_Annenberg.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.annenberg);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Doheny.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.doheny);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_GW.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.gw);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_KDC.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.kdc);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Leavey.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.leavey);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Mudd.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.mudd);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_PE.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.pe);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_SAL.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.sal);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_THH.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.thh);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Tommy.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.tommy);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Traveler.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.traveler);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+        icon_Bookstore.setOnClickListener(v -> {
+            if (!isPopupOpen) {
+                popup_img.setImageResource(R.drawable.bookstore);
+                popup_img.setVisibility(View.VISIBLE);
+                isPopupOpen = true;
+            }
+        });
+
+
+        popup_img.setOnClickListener(v -> {
+            if (isPopupOpen) {
+                popup_img.setVisibility(View.INVISIBLE);
+                isPopupOpen = false;
             }
         });
     }
